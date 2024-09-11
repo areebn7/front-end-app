86c868d7c479de5e7530ee5b1ec1da70c98535c8
Areeb Nabi <anabi001@odu.edu>
2024-09-11 13:17:26 -0400

    Create about.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import CustomerPage from './components/CustomerPage';
 import Layout from './components/Layout';
 import Home from './components/Home'; // Import Home component
+import About from './components/About';
 
 function App() {
   return (
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/customers" element={<CustomerPage />} />
-
+          <Route path="/about" element={<About />} />
           {/* Catch-all Route for undefined URLs */}
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="*" element={<Navigate to="/home" />} />
         </Routes>
       </Layout>
     </Router>
