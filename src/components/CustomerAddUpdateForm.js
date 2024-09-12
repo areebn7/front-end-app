abee610e63a4e061b34faf3ef29d098ca15b8168
Areeb Nabi <anabi001@odu.edu>
2024-09-12 11:53:30 -0400

    reposition form buttons

--- src/components/CustomerAddUpdateForm.js
+++ src/components/CustomerAddUpdateForm.js
@@ -5,65 +5,71 @@ function CustomerAddUpdateForm({ formState, formValues, onInputChange, onSave, o
     <>
       <h2 className="mb-4 mt-4">{formState} Customer</h2>
       <form>
-  <div className="row mb-4">
-    <div className="col-md-2">
-      <label htmlFor="name">Name</label>
-    </div>
-    <div className="col-md-10">
-      <input
-        type="text"
-        className="form-control"
-        id="name"
-        name="name"
-        value={formValues.name}
-        onChange={onInputChange}
-        placeholder="Enter Name"
-        required
-      />
-    </div>
-  </div>
+        <div className="row mb-4">
+          <div className="col-md-2">
+            <label htmlFor="name">Name</label>
+          </div>
+          <div className="col-md-10">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={formValues.name}
+              onChange={onInputChange}
+              placeholder="Enter Name"
+              required
+            />
+          </div>
+        </div>
 
-  <div className="row mb-4">
-    <div className="col-md-2">
-      <label htmlFor="email">Email</label>
-    </div>
-    <div className="col-md-10">
-      <input
-        type="email"
-        className="form-control"
-        id="email"
-        name="email"
-        value={formValues.email}
-        onChange={onInputChange}
-        placeholder="Enter Email"
-        required
-      />
-    </div>
-  </div>
+        <div className="row mb-4">
+          <div className="col-md-2">
+            <label htmlFor="email">Email</label>
+          </div>
+          <div className="col-md-10">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={formValues.email}
+              onChange={onInputChange}
+              placeholder="Enter Email"
+              required
+            />
+          </div>
+        </div>
 
-  <div className="row mb-4">
-    <div className="col-md-2">
-      <label htmlFor="password">Password</label>
-    </div>
-    <div className="col-md-10">
-      <input
-        type="password"
-        className="form-control"
-        id="password"
-        name="password"
-        value={formValues.password}
-        onChange={onInputChange}
-        placeholder="Enter Password"
-        required
-      />
-    </div>
-  </div>
+        <div className="row mb-4">
+          <div className="col-md-2">
+            <label htmlFor="password">Password</label>
+          </div>
+          <div className="col-md-10">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={formValues.password}
+              onChange={onInputChange}
+              placeholder="Enter Password"
+              required
+            />
+          </div>
+        </div>
 
-  <button type="button" className="btn btn-primary " onClick={onSave}>Save</button>
-  <button type="button" className="btn btn-danger m-2" onClick={onDelete}>Delete</button>
-  <button type="button" className="btn btn-secondary" onClick={onCancel}>Cancel</button>
-</form>
-
+        <div className="row">
+          <div className="col-md-12">
+            {/* Button Container aligned to the right */}
+            <div className="d-flex justify-content-end mb-4">
+              <button type="button" className="btn btn-dark mx-1" onClick={onSave}>Save</button>
+              <button type="button" className="btn btn-danger mx-1" onClick={onDelete}>Delete</button>
+              <button type="button" className="btn btn-light mx-1" onClick={onCancel}>Cancel</button>
+            </div>
+          </div>
+        </div>
+      </form>
     </>
   );
 }
